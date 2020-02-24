79a747f69cf9050dbd3c3d82f0479812a3523ae9
victardius <37775040+victardius@users.noreply.github.com>
2020-02-24 15:17:09 +0100

    Testing

---
+++ Assets/Scripts/ImageRecognintionTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+public class ImageRecognintionTest : MonoBehaviour
+{
+    [SerializeField]
+    private ARTrackedImageManager trackedImageManager = null;
+
+
+}
