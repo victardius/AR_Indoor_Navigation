ed4c62394d1310649a649418d2a6e4ec227162e3
victardius <37775040+victardius@users.noreply.github.com>
2020-02-26 13:03:29 +0100

    Testing in progress cube info added

---
+++ Assets/Scripts/AR_DebugTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AR_DebugTest : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cube = null;
+
+    private Text text = null;
+
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (text != null)
+            text.text = "X: " + cube.transform.position.x + "\nY: " + cube.transform.position.y + "\nZ: " + cube.transform.position.z;
+    }
+}
