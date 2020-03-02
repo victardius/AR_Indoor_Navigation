f48f4e88392583bf58743a39b814dc45c1a29e5e
victardius <37775040+victardius@users.noreply.github.com>
2020-03-02 11:28:34 +0100

    KeyPoints added

---
+++ Assets/Scripts/KeyPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPoint : MonoBehaviour
+{
+    [SerializeField]
+    private KeyPointType type = KeyPointType.Start;
+
+    [SerializeField]
+    private GameObject[] nodes = null;
+
+    public GameObject[] Nodes { get { return nodes; } }
+    public KeyPointType Type { get { return type; } }
+}
+
+public enum KeyPointType
+{
+    Start,
+    Left,
+    Right,
+    Forward,
+    End
+}
--- Assets/Scripts/PathManager.cs
+++ Assets/Scripts/PathManager.cs
@@ -9,7 +9,7 @@ public class PathManager : MonoBehaviour
     private int currentObjective = 0;
 
     [SerializeField]
-    private GameObject[] objectives = null;
+    private KeyPoint[] keyPoints = null;
 
     [SerializeField]
     private float detectionDistance = 2f;
@@ -17,20 +17,67 @@ public class PathManager : MonoBehaviour
     [SerializeField]
     private Text text = null;
 
+    [SerializeField]
+    private Transform player = null;
+
+    private KeyPoint currentPoint = null;
+    private GameObject[] objectives = null;
+    private int currentPointIndex = 0;
+
+    private void Awake()
+    {
+        try
+        {
+            currentPoint = keyPoints[currentPointIndex];
+        }catch(NullReferenceException e)
+        {
+            Debug.Log(e);
+        }
+
+        objectives = currentPoint.Nodes;
+    }
+
     private void FixedUpdate()
     {
         if (currentObjective < objectives.Length)
         {
             if (!objectives[currentObjective].activeSelf)
                 objectives[currentObjective].SetActive(true);
-            if (Vector3.Distance(objectives[currentObjective].transform.position, transform.position) < detectionDistance)
+            if (Vector3.Distance(objectives[currentObjective].transform.position, player.position) < detectionDistance)
             {
                 objectives[currentObjective++].SetActive(false);
             }
         }
-        else
+        else if (currentPointIndex < keyPoints.Length - 1)
         {
-            text.text = "You have reached the destination!";
+            currentPoint = keyPoints[++currentPointIndex];
+
+            string message = "";
+
+            switch (currentPoint.Type)
+            {
+                //Play a keypoint type voiceline aswell
+                case KeyPointType.End:
+                    message = "You have reached the destination";
+                    break;
+                case KeyPointType.Left:
+                    message = "Turn left";
+                    break;
+                case KeyPointType.Right:
+                    message = "Turn right";
+                    break;
+                case KeyPointType.Forward:
+                    message = "Continue on straight ahead";
+                    break;
+            }
+
+            text.text = message;
+
+            if (currentPoint.Type != KeyPointType.End)
+            {
+                objectives = currentPoint.Nodes;
+                currentObjective = 0;
+            }
         }
     }
 }
