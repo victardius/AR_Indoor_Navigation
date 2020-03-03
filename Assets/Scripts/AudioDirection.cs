04a05373f3303f83d170ae2d638a267dfa9e8bde
victardius <37775040+victardius@users.noreply.github.com>
2020-03-03 11:23:01 +0100

    Voiclines and audiomanager added

---
+++ Assets/Scripts/AudioDirection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DirectionalVoiceline", menuName = "Voicelines")]
+public class AudioDirection : ScriptableObject
+{
+    [SerializeField]
+    private AudioClip clip = null;
+
+    [SerializeField]
+    private KeyPointType type = KeyPointType.End;
+
+    public AudioClip Voiceline { get; }
+
+    public KeyPointType Type { get; }
+}
