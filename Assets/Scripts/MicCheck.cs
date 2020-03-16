03acb8d7ab1fd73541c39417457570f670eaa18a
CthulhuTheKing <jonathangullstrom@gmail.com>
2020-03-16 17:10:53 +0100

    Mic Check

---
+++ Assets/Scripts/MicCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = Microphone.Start(null, true, 100, 22050);
+        audio.loop = true;
+        while(!(Microphone.GetPosition(null) > 0)) {}
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
